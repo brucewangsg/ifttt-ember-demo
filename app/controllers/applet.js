f747415690793128b292cf9dbbabae0ac704dd30
Bruce Wang <brucewangsg@icloud.com>
2018-01-08 19:39:09 +0800

    click on document to hide popup

--- app/controllers/applet.js
+++ app/controllers/applet.js
@@ -5,6 +5,24 @@ export default Controller.extend({
     toggleThisDropdown() {
       var model = this.get('model'); 
       model.set('showThisDropdown', true);
+
+      // hide popup when click happens on document
+      var cancelShow = (function (model) {
+        return function (ev) {
+          if ($(ev.target).parents('.dropdown-menu:first')[0]) {
+            return;        
+          }
+          $(document).unbind('mousedown', cancelShow);
+          $(document).unbind('touchstart', cancelShow);
+
+          // set flag to false to hide it
+          model.set('showThisDropdown', false);
+        };
+      })(model);
+
+      $(document).bind('mousedown', cancelShow);
+      $(document).bind('touchstart', cancelShow);
+
     }
   }
 });
