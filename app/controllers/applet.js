c9260f56754e3a8d99b71178bc0a817177f9ea82
Bruce Wang <brucewangsg@icloud.com>
2018-01-08 22:17:44 +0800

    choose action scene

--- app/controllers/applet.js
+++ app/controllers/applet.js
@@ -24,6 +24,53 @@ export default Controller.extend({
       var model = this.get('model');
       model.set('selectedTrigger', item);
       model.set('showTriggerDropdown', false);
+    },
+    chooseAction : function (item) {
+      var model = this.get('model');
+      model.set('selectedAction', item);
+      model.set('showActionDropdown', false);
+    },
+    backToTriggerEditing : function () {
+      var model = this.get('model');
+      model.set('isTriggerSceneDone', false);
+    },
+    backToActionEditing : function () {
+      var model = this.get('model');
+      model.set('isActionSceneDone', false);
+    },
+    continueSelectingAction : function () {
+      var model = this.get('model');
+      if (model.get('isValidTrigger')) {
+        model.set('isTriggerSceneDone', true);      
+      }
+    },
+    setTriggerAttribute : function (key, value) {
+      var model = this.get('model');
+      model.set('triggerAttributes.' + key, value);      
+      model.set(key + 'FocusOut', true);
+
+      if (key == 'address') {
+        model.verifyAddress();
+      }
+    },
+    finalizeAction : function () {
+      var model = this.get('model');
+      if (model.get('isValidAction')) {
+        model.set('isActionSceneDone', true);      
+      }
+    },
+    setActionAttribute : function (key, value) {
+      var model = this.get('model');
+      model.set('actionAttributes.' + key, value);      
+      model.set(key + 'FocusOut', true);
+
+      if (key == 'email') {
+        model.verifyEmail();
+      }
+    },
+    saveAndSubmit : function () {
+      var model = this.get('model');
+      model.set('isAllDone', true);
     }
   }
 });
